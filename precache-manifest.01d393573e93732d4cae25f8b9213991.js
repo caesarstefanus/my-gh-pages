5c74b538d1b7a49ee61dba2b99e0cbac26363ee2
caesarstefanus <caesarstefanus@gmail.com>
2020-08-10 14:15:23 +0900

    Updates

---
+++ precache-manifest.01d393573e93732d4cae25f8b9213991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7312eda4ec031d590ddd1c8b52c36297",
+    "url": "/my-gh-pages/index.html"
+  },
+  {
+    "revision": "4de8ee7feb3ca9f68033",
+    "url": "/my-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5966e0b161e97287cb7b",
+    "url": "/my-gh-pages/static/js/2.81a9ca4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-gh-pages/static/js/2.81a9ca4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de8ee7feb3ca9f68033",
+    "url": "/my-gh-pages/static/js/main.646662a9.chunk.js"
+  },
+  {
+    "revision": "ee3fc6a48f97c18321ee",
+    "url": "/my-gh-pages/static/js/runtime-main.fea5b825.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
